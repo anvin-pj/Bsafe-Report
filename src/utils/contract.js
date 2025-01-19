522260fa06966cb619b0e366ac2002919a6f911e
abelboby <abeldavisboby@gmail.com>
2025-01-19 08:15:47 +0530

    changed everything to reclaim

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -92,101 +92,8 @@ const contractABI = [
 		"type": "function"
 	},
 	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			}
-		],
-		"name": "withdraw",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			}
-		],
-		"name": "Withdrawal",
-		"type": "event"
-	},
-	{
 		"stateMutability": "payable",
 		"type": "receive"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_reporter",
-				"type": "address"
-			}
-		],
-		"name": "getReportsByAddress",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "uint256",
-						"name": "id",
-						"type": "uint256"
-					},
-					{
-						"internalType": "address",
-						"name": "reporter",
-						"type": "address"
-					},
-					{
-						"internalType": "string",
-						"name": "description",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "location",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "evidenceLink",
-						"type": "string"
-					},
-					{
-						"internalType": "bool",
-						"name": "verified",
-						"type": "bool"
-					},
-					{
-						"internalType": "uint256",
-						"name": "reward",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "timestamp",
-						"type": "uint256"
-					}
-				],
-				"internalType": "struct ReportAndReward.ReportData[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
 	},
 	{
 		"inputs": [],
@@ -196,19 +103,6 @@ const contractABI = [
 				"internalType": "address",
 				"name": "",
 				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "reportCount",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
 			}
 		],
 		"stateMutability": "view",
@@ -269,7 +163,7 @@ const contractABI = [
 		"type": "function"
 	}
 ]
-  const contractAddress = '0xc29ee1fe0178c2ba74bf13791151dafc7ac1fab5';
+  const contractAddress = '0xdda1587c4c15fbc16f7b03c1be94e687aca1a1ba';
   
   export { contractABI, contractAddress };
   
